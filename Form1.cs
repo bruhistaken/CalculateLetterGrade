c9c6b52cf941a6f232cfad47a2ff5825abb71baa
tim <96191984+bruhistaken@users.noreply.github.com>
2023-09-14 10:52:04 -0400

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-namespace AreaAndPerimeter
+namespace CalculateLetterGrade
 {
     public partial class Form1 : Form
     {
@@ -7,13 +7,38 @@ namespace AreaAndPerimeter
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            int pmtr = (Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text)) * 2;
-            int area = Convert.ToInt32(textBox1.Text) * Convert.ToInt32(textBox2.Text);
 
-            textBox3.Text = pmtr.ToString();
-            textBox4.Text = area.ToString();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double numericGrade = Convert.ToDouble(txtNumberGrade.Text);
+
+            string letterGradeValue;
+            if (numericGrade >= 90.0)
+            {
+                letterGradeValue = "A";
+            }
+            else if (numericGrade >= 80.0)
+            {
+                letterGradeValue = "B";
+            }
+            else if (numericGrade >= 70.0)
+            {
+                letterGradeValue = "C";
+            }
+            else if (numericGrade >= 60.0)
+            {
+                letterGradeValue = "D";
+            }
+            else
+            {
+                letterGradeValue = "F";
+            }
+
+            txtLetterGrade.Text = letterGradeValue;
 
         }
     }
